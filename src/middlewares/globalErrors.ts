f4f8436b20b4c51c31114a8b50e175bd4fdb901c
LeonardoOliveira3010 <65436800+LeonardoOliveira3010@users.noreply.github.com>
2021-12-07 22:59:35 -0300

    Criação dos middlewares

---
+++ src/middlewares/globalErrors.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction, response } from "express";
+import AppError from '../shared/error/AppError'
+
+function globalErrors(err: Error, req: Request, res: Response, next: NextFunction){
+    if(err instanceof AppError) {
+        response.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+            data: err?.data
+        })
+    }
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Erro do servidor interno'
+    })
+}
+
+export {globalErrors}
